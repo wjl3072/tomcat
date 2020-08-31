10b591c8868191e03d985827f798ce9265e362df
Administrator <3072451452qq.com>
2020-08-31 15:28:44 +0800

    div分支

--- src/cn/git/Text.java
+++ src/cn/git/Text.java
@@ -5,6 +5,7 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
+        System.out.println("div分支");
     }
 
 }
